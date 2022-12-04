ef3bca65ef64373fce6ea162fce200f10afc7adf
Christian Jae Ronque <ronquechristian@gmail.com>
2022-12-04 18:23:31 +0800

    tilt

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
-//#region ----- Elements -----
+//#region ----- Variables -----
+// Elements
 const logosSectionEl = document.querySelector(".section-logos");
 const challengesSectionEl = document.querySelector(".challenge-section");
 const exerciseLinksEl = document.querySelectorAll(".exercise-links");
@@ -26,6 +27,14 @@ const htmlHeadingsContainerEl = document.querySelector(".html__headings");
 const htmlParagraphContainerEl = document.querySelector(".html__paragraph");
 const htmlCommentsContainerEl = document.querySelector(".html__comments");
 
+// Anser input
+const answerInput = document.querySelector(".answer-input");
+
+const attQuestion1El = document.querySelector(".att-q--1");
+
+// Submit buttons
+const btnSubmitAnsEl = document.querySelectorAll(".submit-answer__btn");
+
 // Question box container
 const questionBoxContainerAllEl = document.querySelectorAll(
   ".question-box-container"
@@ -37,6 +46,21 @@ const questionBoxContainerEl = document.querySelector(
 let dark = "moon-sharp",
   light = "sunny-outline",
   mode = dark;
+
+//#region ----- Challenges Objects -----
+
+// HTML
+const answersHtml = {
+  htmlAttribute: {
+    1: { a: "title" },
+    2: { a: "250px", b: "400px" },
+    3: { a: "href" },
+    4: { a: "alt" },
+  },
+};
+
+const answers = [answersHtml];
+//#endregion
 //#endregion
 
 //#region ----- Functions -----
@@ -50,7 +74,6 @@ const hideContentToggle = function (topic, e) {
   const clicked = e.target.closest(".exercise-links");
 
   if (!clicked) return;
-  smoothScroll(questionBoxContainerEl);
 
   questionBoxContainerAllEl.forEach((c) => c.classList.add("hide-content"));
 
@@ -118,4 +141,79 @@ htmlCommentsContainerEl.addEventListener("click", (e) => {
   hideContentToggle("comments", e);
 });
 //#endregion
+
+// Submit button event handler
+
 //#endregion
+
+// PUTANGINAMO
+/*
+btnSubmitAnsEl.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const clicked = e.target;
+    let obj = clicked.dataset.tab;
+
+    let input = answerInput.value;
+    if (
+      input === answers[0].htmlAttribute[obj].a ||
+      answers[0].htmlAttribute[obj].b
+    ) {
+      clicked.style.backgroundColor = "green";
+      clicked.textContent = "Proceed";
+
+      // Proceed button
+      clicked.addEventListener("click", () => {
+        questionBoxContainerAllEl.forEach((c) =>
+          c.classList.add("hide-content")
+        );
+        document
+          .querySelector(`.h-attributes-q--${Number(clicked.dataset.tab) + 1}`)
+          .classList.remove("hide-content");
+        input = answerInput.value = "";
+      });
+      obj++;
+    } else {
+      clicked.style.backgroundColor = "red";
+      clicked.textContent = "Try again!";
+    }
+    console.log(input);
+  })
+);
+*/
+const btnAtt1 = document.querySelector(".att-submit--1");
+const btnAtt2 = document.querySelector(".att-submit--2");
+
+btnAtt1.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const clicked = e.target;
+  const answer1 = document.querySelector(".att-1--a");
+
+  if (answer1.value === answers[0].htmlAttribute[1].a) {
+    btnAtt1.style.backgroundColor = "green";
+    btnAtt1.textContent = "Mark as done";
+  } else {
+    btnAtt1.style.backgroundColor = "red";
+    btnAtt1.textContent = "Try again";
+  }
+});
+
+btnAtt2.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const clicked = e.target;
+  const answer1 = document.querySelector(".att-2--a");
+  const answer2 = document.querySelector(".att-2--b");
+
+  if (
+    answer1.value === answers[0].htmlAttribute[2].a &&
+    answer2.value === answers[0].htmlAttribute[2].b
+  ) {
+    btnAtt2.style.backgroundColor = "green";
+    btnAtt2.textContent = "Mark as done";
+  } else {
+    btnAtt2.style.backgroundColor = "red";
+    btnAtt2.textContent = "Try again";
+  }
+});
